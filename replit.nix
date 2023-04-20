08a1e528901329f9181a08120963e141b0deb5a5
RadleighPompei1 <21055135-RadleighPompei1@users.noreply.replit.com>
2023-04-20 07:18:41 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,12 @@
+{ pkgs }: {
+    deps = [
+        pkgs.htop
+        pkgs.cups
+        pkgs.busybox
+        pkgs.bashInteractive
+        pkgs.man
+        pkgs.caddy
+        pkgs.tmux
+        pkgs.jre8
+    ];
+}
